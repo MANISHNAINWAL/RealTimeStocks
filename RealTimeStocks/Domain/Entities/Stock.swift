26bbef91c699dce7f4766ba626e3a968e9073143
MANISHNAINWAL <manishnainwal1@gmail.com>
2026-02-28 22:57:42 +0530

    extended Stock
    added change and ispositive to check the change and whether a stock is positive or not

--- RealTimeStocks/Domain/Entities/Stock.swift
+++ RealTimeStocks/Domain/Entities/Stock.swift
@@ -5,10 +5,33 @@
 //  Created by Manish Nainwal on 27/02/26.
 //
 
-public struct Stock: Identifiable{
+import Foundation
+
+public struct Stock: Identifiable, Equatable,Hashable{
     public let id: String
     public let symbol: String
     public let description: String
     public var price: Double
-
+    public var previousPrice: Double?
+    
+    public init(id: String,
+                symbol: String,
+                description: String,
+                price: Double,
+                previousPrice: Double? = nil) {
+        self.id = id
+        self.symbol = symbol
+        self.description = description
+        self.price = price
+        self.previousPrice = previousPrice
+    }
+    
+    public var change: Double {
+        guard let previousPrice else { return 0 }
+        return price - previousPrice
+    }
+    
+    public var isPositive: Bool {
+        change >= 0
+    }
 }
